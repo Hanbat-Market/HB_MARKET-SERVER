85a612ff7b0268402ad148833d0d8a4504d038e8
jckim22 <jckim229@gmail.com>
2024-01-29 16:06:36 +0900

    Init: 프로젝트 초기 세팅
    스프링 부트 초기세팅

---
+++ src/main/java/HM/Hanbat_Market/HanbatMarketApplication.java
@@ -0,0 +1,13 @@
+package HM.Hanbat_Market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanbatMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanbatMarketApplication.class, args);
+	}
+
+}
