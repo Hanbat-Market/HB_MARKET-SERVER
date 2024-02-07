4d6b5dd78cfdd8b7b52a1d10e36d631462ac183f
Kim_ju_chan <jckim229@gmail.com>
2024-02-07 15:11:36 +0900

    ✨ Feat: 관심 상품 관련 비즈니스 로직 구현 및 테스트 (#27)
    - 관심상품 상태 추가
      - PreemptionItemStatus (PREEMPTION, CANCEL)
    - 관심상품 클래스 비즈니스 로직 추가
      - regisPreemptionItem
      - active
      - cancel
    - PreemptionItemService 추가
    
    Resolves: #27

---
+++ src/test/java/HM/Hanbat_Market/service/preemption/PreemptionItemServiceTest.java
@@ -0,0 +1,68 @@
+package HM.Hanbat_Market.service.preemption;
+
+import HM.Hanbat_Market.domain.entity.*;
+import HM.Hanbat_Market.repository.item.ItemRepository;
+import HM.Hanbat_Market.repository.item.PreemptionItemRepository;
+import HM.Hanbat_Market.service.article.ArticleService;
+import HM.Hanbat_Market.service.member.MemberService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static HM.Hanbat_Market.CreateTestEntity.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class PreemptionItemServiceTest {
+    @Autowired
+    PreemptionItemService preemptionItemService;
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    ArticleService articleService;
+    @Autowired
+    ItemRepository itemRepository;
+    @Autowired
+    PreemptionItemRepository preemptionItemRepository;
+
+    @Test
+    public void 관심_상품_등록() throws Exception {
+        //given
+        Member testMember = createTestMember1();
+        memberService.join(testMember);
+
+        Long articleId = articleService.regisArticle(testMember.getId(), createArticleCreateDto("test title", "test des", "대전"),
+                createItemCreateDto("플스", 170000L));
+        Article article = articleService.findArticle(articleId);
+        Item item = itemRepository.findById(article.getItem().getId()).get();
+        //when
+        Long preemptionItemId = preemptionItemService.regisPreemption(testMember.getId(), item.getId());
+        //then
+        PreemptionItem preemptionItem = preemptionItemRepository.findById(preemptionItemId).get();
+
+        assertEquals(preemptionItem, preemptionItemService.findPreemptionItemByMember(testMember).get(0));
+        assertEquals(preemptionItem, preemptionItemService.findPreemptionItemByItem(item).get(0));
+        assertEquals(PreemptionItemStatus.PREEMPTION, preemptionItem.getPreemptionItemStatus());
+    }
+
+    @Test
+    public void 관심_상품_취소() throws Exception {
+        //given
+        Member testMember = createTestMember1();
+        memberService.join(testMember);
+
+        Long articleId = articleService.regisArticle(testMember.getId(), createArticleCreateDto("test title", "test des", "대전"),
+                createItemCreateDto("플스", 170000L));
+        Article article = articleService.findArticle(articleId);
+        Item item = itemRepository.findById(article.getItem().getId()).get();
+        Long preemptionItemId = preemptionItemService.regisPreemption(testMember.getId(), item.getId());
+        PreemptionItem preemptionItem = preemptionItemRepository.findById(preemptionItemId).get();
+        //when
+        preemptionItemService.cancelPreemption(preemptionItemId);
+        //then
+        assertEquals(0, preemptionItemService.findAllPreemptionItem().size());
+        assertEquals(PreemptionItemStatus.CANCEL, preemptionItem.getPreemptionItemStatus());
+    }
+}
